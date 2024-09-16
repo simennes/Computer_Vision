b31b83aca1e2ff398478bde20c7cd266b6976c16
simennes <93517947+simennes@users.noreply.github.com>
2024-09-16 11:43:42 +0200

    Add files via upload

---
+++ ComputerVisionPoolTable/build/CMakeFiles/finalProject.dir/cmake_clean.cmake
@@ -0,0 +1,25 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/finalProject.dir/src/BallDetector.cpp.o"
+  "CMakeFiles/finalProject.dir/src/BallDetector.cpp.o.d"
+  "CMakeFiles/finalProject.dir/src/BallTracker.cpp.o"
+  "CMakeFiles/finalProject.dir/src/BallTracker.cpp.o.d"
+  "CMakeFiles/finalProject.dir/src/HelperFunctions.cpp.o"
+  "CMakeFiles/finalProject.dir/src/HelperFunctions.cpp.o.d"
+  "CMakeFiles/finalProject.dir/src/MeanAveragePrecision.cpp.o"
+  "CMakeFiles/finalProject.dir/src/MeanAveragePrecision.cpp.o.d"
+  "CMakeFiles/finalProject.dir/src/MeanIntersectionOverUnion.cpp.o"
+  "CMakeFiles/finalProject.dir/src/MeanIntersectionOverUnion.cpp.o.d"
+  "CMakeFiles/finalProject.dir/src/TableDetector.cpp.o"
+  "CMakeFiles/finalProject.dir/src/TableDetector.cpp.o.d"
+  "CMakeFiles/finalProject.dir/src/TopViewVisual.cpp.o"
+  "CMakeFiles/finalProject.dir/src/TopViewVisual.cpp.o.d"
+  "CMakeFiles/finalProject.dir/src/main.cpp.o"
+  "CMakeFiles/finalProject.dir/src/main.cpp.o.d"
+  "finalProject"
+  "finalProject.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/finalProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
